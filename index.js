f8928319f14020900a68b1e0954c46c71aac9653
VISHALSHARMAVS <vishalsharmakt143@gamil.com>
2024-02-06 01:14:05 +0530

    Error Handling for wrong city name

--- index.js
+++ index.js
@@ -6,14 +6,18 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 const searchCity = document.querySelector(".mySearch input");
 const searchButton = document.querySelector(".mySearch button");
 const weatherIcon = document.querySelector(".weatherIcon");
-
-
+const errorMsg = document.querySelector(".error");
+const weather= document.querySelector(".weather");
 async function checkWeather(city){
     const response = await fetch(apiUrl+city+`&appid=${apiKey}`)
     var data = await response.json();
    
+if (response.status == 404){
+   errorMsg.style.display ="block";
+weather.style.display="none";
 
-
+}
+else{
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity+"%";
@@ -22,9 +26,12 @@ async function checkWeather(city){
   const weatherImg = data.weather[0].main;
  weatherIcon.src = "images/"+`${weatherImg}`+".png";
 
-
-document.querySelector(".weather").style.display="block";
+errorMsg.style.display="none";
+weather.style.display="block";
 }
+}
+
+   
 searchButton.addEventListener("click",()=>{
 
     checkWeather(searchCity.value);
